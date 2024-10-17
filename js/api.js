d111848ee599046d2fddd24fa6269d5b8a9da368
Nikon Veremeichik <thesecondnikon@gmail.com>
2024-10-17 07:05:58 +0500

    quickfix bug in api

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@ const green_lake = document.getElementById("Green Lake")
 async function Api() {
     const respons_petro = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=69.11548809207305&current=temperature_2m&hourly=temperature_2m")
     const respons_tucson = await fetch("https://api.open-meteo.com/v1/forecast?latitude=32.388700047778265&longitude=-111.09217947996893&current=temperature_2m&hourly=temperature_2m")
-    const respons_tokyo = await fetch("https://api.open-meteo.com/v1/forecast?latitude=32.388700047778265&longitude=-111.09217947996893&current=temperature_2m&hourly=temperature_2m")
+    const respons_tokyo = await fetch("https://api.open-meteo.com/v1/forecast?latitude=35.45241385930772&longitude=139.69801824402919&current=temperature_2m&hourly=temperature_2m")
     const respons_akranes = await fetch("https://api.open-meteo.com/v1/forecast?latitude=32.388700047778265&longitude=-111.09217947996893&current=temperature_2m&hourly=temperature_2m")
     const respons_green_lake = await fetch("https://api.open-meteo.com/v1/forecast?latitude=32.388700047778265&longitude=-111.09217947996893&current=temperature_2m&hourly=temperature_2m")
     const data_petro = await respons_petro.json()
@@ -15,7 +15,7 @@ async function Api() {
     const data_tokyo = await respons_tokyo.json()
     const data_akranes = await respons_akranes.json()
     const data_green_lake = await respons_green_lake.json()
-    return [data_petro. data_tucson, data_tokyo, data_akranes, data_green_lake]
+    return [data_petro, data_tucson, data_tokyo, data_akranes, data_green_lake]
 }
 
 async function output() {
